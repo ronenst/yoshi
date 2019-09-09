3bfe17c642affbe5b1f8b3b69a470630d2bcc8f1
Ronen Stern <ronens@wix.com>
2019-09-09 10:01:44 +0300

    Fix JS template to match TS template

--- packages/create-yoshi-app/templates/business-manager-module/typescript/src/config.ts
+++ packages/create-yoshi-app/templates/business-manager-module/typescript/src/config.ts
@@ -4,10 +4,10 @@ import {
   TModuleParams,
 } from '@wix/business-manager-api';
 
-// Note: after submitting PR to BM you can remove the constant strings below
+// Note: after submitting PR to BM you can replace with the constant strings below
 // Note: should be in sync with the config inside your BM module ERB
-export const MODULE_ID = /*ModuleId.{%ProjectName%} ||*/ '{%PROJECT_NAME%}' as ModuleId;
-export const COMPONENT_NAME = /*PageComponentId.{%ProjectName%} ||*/ '{%projectName%}' as PageComponentId;
+export const MODULE_ID = /*ModuleId.{%ProjectName%}*/ '{%PROJECT_NAME%}' as ModuleId;
+export const COMPONENT_NAME = /*PageComponentId.{%ProjectName%}*/ '{%projectName%}' as PageComponentId;
 
 export interface IERBConfig {
   topology: {
